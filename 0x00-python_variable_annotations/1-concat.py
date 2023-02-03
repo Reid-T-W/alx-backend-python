0555b442815b713a9989c0a6bb39dd0019335d67
Reid-T-W <rediettadesse100@gmail.com>
2023-02-04 00:01:15 +0300

    string annotated params and return types

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Type annotated function concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat function with string annotations
+    for param and return types
+    """
+    return (str1 + str2)
