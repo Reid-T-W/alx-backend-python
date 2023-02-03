fa4b5aa945a7abccf002915f843792f13c45342d
Reid-T-W <rediettadesse100@gmail.com>
2023-02-04 00:30:55 +0300

    function annotation for float argument and string return type

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Type-annotated function to_str """
+
+
+def to_str(n: float) -> str:
+    """
+    to_str accepts a float argument n
+    and returns a string format of the
+    n
+    """
+    return str(n)
