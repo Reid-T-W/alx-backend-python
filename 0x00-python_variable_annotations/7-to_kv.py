894780433cd37a150ea3d0cb6a430c71af2693cb
Reid-T-W <rediettadesse100@gmail.com>
2023-02-04 02:38:16 +0300

    Pycodestyle satisfied

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,7 +4,7 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """ 
+    """
     Function with type-arguments str,
     Union and Tuple
     """
