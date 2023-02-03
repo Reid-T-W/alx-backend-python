f3e68d67412ecc84d501f0d7063a3b388954362b
Reid-T-W <rediettadesse100@gmail.com>
2023-02-04 00:07:03 +0300

    annotated function that returns an int from the floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Type annotated function floor """
+import math
+
+
+def floor(n: float) -> int:
+    """
+    floor annotated function that takes
+    float as argument and retuns an int
+    """
+    return math.floor(n)
