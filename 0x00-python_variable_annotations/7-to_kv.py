4111993b92311e98b7c3c5e7d47ca1ac79ca97ef
Reid-T-W <rediettadesse100@gmail.com>
2023-02-04 02:37:11 +0300

    Docstring added for function

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,4 +4,8 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ 
+    Function with type-arguments str,
+    Union and Tuple
+    """
     return (k, pow(v, 2))
