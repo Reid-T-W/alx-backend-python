5d09dbe6ba37f03f3b8fcaaf7b074705409b61dd
Reid-T-W <rediettadesse100@gmail.com>
2023-02-04 13:05:15 +0300

    array type is converted from list to tuple

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,7 +5,7 @@
 from typing import Tuple, List
 
 
-def zoom_array(lst: List, factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Type-annotated function that takes in Tuple and
     int as argument and returns a Tuple
@@ -17,7 +17,7 @@ def zoom_array(lst: List, factor: int = 2) -> List:
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = tuple([12, 72, 91])
 
 zoom_2x = zoom_array(array)
 
