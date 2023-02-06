4a3b5fb4caf0d4f7d5fcea0e1b936f474f320b3d
Reid-T-W <rediettadesse100@gmail.com>
2023-02-06 17:55:51 +0300

    random variable type annotated

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,5 +5,5 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """ Asynchronous corotine that returs a random delay """
-    random_delay = random.uniform(0, max_delay)
+    random_delay: float = random.uniform(0, max_delay)
     return random_delay
