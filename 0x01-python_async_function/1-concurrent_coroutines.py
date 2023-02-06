44d84238cffaa91fc32642878a61ce7a23f6ac0b
Reid-T-W <rediettadesse100@gmail.com>
2023-02-06 21:42:34 +0300

    Callable import removed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Execute multiple coroutines at the same time """
-from typing import Callable
 import asyncio
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
