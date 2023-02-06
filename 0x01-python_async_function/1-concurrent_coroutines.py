607ff84a73e36894d2786f3b651e6925ceda0557
Reid-T-W <rediettadesse100@gmail.com>
2023-02-06 21:26:42 +0300

    As completed used instead of gather

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Execute multiple coroutines at the same time """
 from typing import Callable
-
+import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -9,5 +9,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> [float]:
     """ Execute multiple coroutines """
     delays = []
-    delays = [await wait_random(max_delay) for i in range(n)]
-    return delays
+    results = []
+    delays = [wait_random(max_delay) for i in range(n)]
+    for res in asyncio.as_completed(delays):
+        results.append(await res)
+    return results
