54cb845c2067fa564886eac0c609e8e98fd8cd18
Reid-T-W <rediettadesse100@gmail.com>
2023-02-07 12:43:54 +0300

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Async Generator """
+import random
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ Async Generator """
+    for i in range(10):
+        await asyncio.sleep(1)
+        random_no = random.uniform(0, 10)
+        yield random_no
