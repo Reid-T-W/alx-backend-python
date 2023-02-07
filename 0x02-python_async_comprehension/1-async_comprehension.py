a14acf6d9c1e8081ef12e25025511ee7caac1779
Reid-T-W <rediettadesse100@gmail.com>
2023-02-07 13:35:14 +0300

    Docstring added to async function

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,6 +5,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """ Async coroutine that collects data from async generator """
     result = []
     result = [num async for num in async_generator()]
     return result
