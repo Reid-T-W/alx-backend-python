297f7cc77e5f71b074bde60d951c850b421740a7
Reid-T-W <rediettadesse100@gmail.com>
2023-02-07 04:38:54 +0300

    return type annotation changed

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,6 +7,6 @@ from typing import Awaitable
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable[int]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ Regular function that returns an asyncio task """
     return (asyncio.create_task(wait_random(max_delay)))
