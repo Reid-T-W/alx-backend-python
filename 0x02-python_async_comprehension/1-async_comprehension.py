226443f7fd8d4435595d378b245bf05ba9a184ef
Reid-T-W <rediettadesse100@gmail.com>
2023-02-07 13:33:33 +0300

    List comprhenstion used instead of a regular for loop

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ Async coroutine that collects data from async generator """
-from typing import Generator, List
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     result = []
-    async for num in async_generator():
-        result.append(num)
+    result = [num async for num in async_generator()]
     return result
